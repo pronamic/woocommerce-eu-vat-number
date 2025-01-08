bbca9e966e5f0e9bedf1ff3b350823063669d515
WooCommerce <info@woocommerce.com>
2025-01-08 10:14:46 +0000

    Updates to 2.9.12

--- build/frontend.asset.php
+++ build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-settings', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '97a3499acec11db8209d');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-settings', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8c208056ae751b58d574');
